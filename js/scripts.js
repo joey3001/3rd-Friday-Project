acca1876768bb762383b7f33f777920333b0389d
Joey3001 <josephnero111@gmail.com>
2020-10-30 11:24:18 -0700

    Fix a bug where the number 1 was not swapped to 'Beep'git log

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,11 @@ $(document).ready(function() {
     let numberArray = []; 
     for (let i = 0; i <= number; i++) {
       numberArray[i] = i; 
-      if (i > 9) {
-        let digits = i.toString().split("");
-        for (let index = 0; index <= digits.length; index++) {
-          if (digits[index] === "1") {
-            numberArray[i] = "beep";
-            break;
-          }
+      let digits = i.toString().split("");
+      for (let index = 0; index <= digits.length; index++) {
+        if (digits[index] === "1") {
+          numberArray[i] = "Beep!";
+          break;
         }
       }
     }
