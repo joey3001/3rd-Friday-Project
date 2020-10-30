39bffe5409ba2a2a9270bd6597e2d7cb91ca2f03
Joey3001 <josephnero111@gmail.com>
2020-10-30 12:14:06 -0700

    Separate business logic from UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
+function numberSwapper(userNumber) {
+  let numberSwapArray = [];
+  for (let i = 0; i <= userNumber; i++) {
+    numberSwapArray[i] = " ".concat(i.toString()); 
+    let individualDigits = i.toString();
+      if (individualDigits.indexOf("3") >= 0) {
+        numberSwapArray[i] = " Won't you be my neighbor?";
+      }
+      else if (individualDigits.indexOf("2") >= 0) {
+        numberSwapArray[i] = " Boop!";
+      }
+      else if (individualDigits.indexOf("1") >= 0) {
+        numberSwapArray[i] = " Beep!";
+      }
+    }
+  return numberSwapArray
+}
+
+
 $(document).ready(function() {
   $("#numberInput").submit(function(event){
     event.preventDefault();
-    let number = $("#number").val();
-    let numberArray = []; 
-    for (let i = 0; i <= number; i++) {
-      numberArray[i] = " ".concat(i.toString()); 
-      let individualDigits = i.toString();
-        if (individualDigits.indexOf("3") >= 0) {
-        numberArray[i] = " Won't you be my neighbor?";
-        }
-        else if (individualDigits.indexOf("2") >= 0) {
-          numberArray[i] = " Boop!";
-        }
-        else if (individualDigits.indexOf("1") >= 0) {
-          numberArray[i] = " Beep!";
-        }
-      }
+    let numberArray = numberSwapper($("#number").val());
     alert(numberArray);
   });
 });
