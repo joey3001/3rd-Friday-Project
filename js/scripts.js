7ba5fcebfba815e934eb293ad5f565520548a838
Joey3001 <josephnero111@gmail.com>
2020-10-30 10:04:01 -0700

    Use a for loop to store all numbers up to and including the user inputted number in an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $("#numberInput").submit(function(event){
     event.preventDefault();
+    let number = $("#number").val();
+    let numberArray = []; 
+    for (let i = 0; i <= number ; i++) {
+      numberArray[i] = i 
+      alert(numberArray);
+    }
   });
 });
