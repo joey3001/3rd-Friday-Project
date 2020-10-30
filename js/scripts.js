443b3546b0ca9257f6ffba21609b3b48bc413ade
Joey3001 <josephnero111@gmail.com>
2020-10-30 12:57:04 -0700

    Fix indenation and remove redundant div tag

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ function numberSwapper(userNumber) {
   let numberSwapArray = [];
   for (let i = 0; i <= userNumber; i++) {
     numberSwapArray[i] = " ".concat(i.toString()); 
-      if (i.toString().indexOf("3") >= 0) {
-        numberSwapArray[i] = " Won't you be my neighbor?";
-      }
-      else if (i.toString().indexOf("2") >= 0) {
-        numberSwapArray[i] = " Boop!";
-      }
-      else if (i.toString().indexOf("1") >= 0) {
-        numberSwapArray[i] = " Beep!";
-      }
+    if (i.toString().indexOf("3") >= 0) {
+      numberSwapArray[i] = " Won't you be my neighbor?";
     }
-  return numberSwapArray
+    else if (i.toString().indexOf("2") >= 0) {
+      numberSwapArray[i] = " Boop!";
+    }
+    else if (i.toString().indexOf("1") >= 0) {
+      numberSwapArray[i] = " Beep!";
+    }
+  }
+  return numberSwapArray;
 }
-
 
 $(document).ready(function() {
   $("#numberInput").submit(function(event) {
